7186065e5f03f5a2bbd00aaaead191fe85d90c21
eyad <eyiadhisham@yahoo.com>
2019-12-25 21:30:00 +0200

    set start point

--- src/app/models/maze.ts
+++ src/app/models/maze.ts
@@ -9,6 +9,10 @@ export class Maze
   height:number;
   maze:Tile[] = [];
 
+  public get size() : number {
+    return this.width*this.height;
+  }
+
   constructor(width:number,height:number)
   {
     this.width = width;
@@ -17,7 +21,7 @@ export class Maze
       for (let y = 0; y < height; y++)
         this.maze.push(new Tile(State.open,new Point(x,y)));
 
-
+    this.SetStartPoint(new Point(0,0));
     this.TileAt(1,1).state = State.blocked;
     this.TileAt(2,1).state = State.blocked;
     this.TileAt(3,1).state = State.blocked;
@@ -26,11 +30,15 @@ export class Maze
     this.TileAt(3,3).state = State.blocked;
     this.TileAt(3,4).state = State.goal;
   }
-
+  SetStartPoint(point:Point)
+  {
+    if(!this.isInsideMaze(point))
+      throw `point ${point}`;
+    this.start = this.maze[this.pointTo1D(point)];
+  }
   TileAt(x:number,y:number) :Tile
   {
     let index = this.to1D(x,y);
-    console.log(this.maze.length);
     return this.maze[index];
   }
   Draw()
@@ -41,45 +49,30 @@ export class Maze
       for (let y = 0; y < this.height; y++)
       {
         let index = this.to1D(x,y);
-        console.log(`(${x},${y}) => ${index}`);
-
         let tile = this.maze[index];
-        if (tile.state == State.open)
+        if(tile ==  this.start)
+          mazeTxt+='S';
+        else if (tile.state == State.open)
           mazeTxt+='-';
-        if (tile.state == State.goal)
+        else if (tile.state == State.goal)
             mazeTxt+='G';
-        if (tile.state == State.blocked)
+        else if (tile.state == State.blocked)
           mazeTxt+='x';
       }
       mazeTxt+='\n';
     }
-      // debugger
       console.log(mazeTxt);
   }
-
+  isInsideMaze(point:Point) : boolean
+  {
+    return this.pointTo1D(point)<this.size;
+  }
+  pointTo1D(point:Point) : number
+  {
+    return this.to1D(point.x,point.y);
+  }
   to1D(x:number,y:number) : number
   {
     return y*this.width+x;
   }
-  // solveMaze()
-  // {
-  //   let start = new Point(0,0);
-
-  //   let curPath:Point[] = [start];
-  //   let visitedPath:Point[] = [start];
-
-  //   for (let rowIndex = 0; rowIndex < this.maze.length; rowIndex++)
-  //   {
-  //     const row = this.maze[rowIndex];
-  //     for (let colIndex = 0; colIndex < row.length; colIndex++)
-  //     {
-  //       const cell = row[colIndex];
-  //       let curPoint = new Point(rowIndex,colIndex);
-  //       let tileState = cell;
-  //       if(tileState==0)
-  //         curPath.push(curPoint);
-  //       else
-  //       {}
-  //     }
-  //   }
 }
