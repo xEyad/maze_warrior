b8993449823f0db4fd8e48c6fce11a85a337f475
eyad <eyiadhisham@yahoo.com>
2019-12-25 22:07:38 +0200

    world can be drawn

--- src/app/models/world.ts
+++ src/app/models/world.ts
@@ -3,27 +3,29 @@ import { State } from './tile';
 import { Maze } from './maze';
 import { Point } from './point';
 
-class World
+///only 1 per program
+export class World implements iDrawable
 {
-  CreateMaze() : Maze
+  maze:Maze;
+  constructor()
   {
-    let maze = new Maze(4,5);
-    maze.SetStartPoint(new Point(0,0));
-    maze.TileAt(1,1).state = State.blocked;
-    maze.TileAt(2,1).state = State.blocked;
-    maze.TileAt(3,1).state = State.blocked;
-    maze.TileAt(1,3).state = State.blocked;
-    maze.TileAt(1,4).state = State.blocked;
-    maze.TileAt(3,3).state = State.blocked;
-    maze.TileAt(3,4).state = State.goal;
-    return maze;
+    this.maze = new Maze(4,5);
+    this.maze.SetStartPoint(new Point(0,0));
+    this.maze.TileAt(1,1).state = State.blocked;
+    this.maze.TileAt(2,1).state = State.blocked;
+    this.maze.TileAt(3,1).state = State.blocked;
+    this.maze.TileAt(1,3).state = State.blocked;
+    this.maze.TileAt(1,4).state = State.blocked;
+    this.maze.TileAt(3,3).state = State.blocked;
+    this.maze.TileAt(3,4).state = State.goal;
   }
   Run():void
   {
-    let maze = this.CreateMaze();
-    let walker = new Walker(maze.start.coordinate);
+    let walker = new Walker(this.maze.start.coordinate);
+  }
+  Draw()
+  {
+    this.maze.Draw();
   }
 }
 
-let world = new World();
-export{world};
