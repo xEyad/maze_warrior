10e29ff720be06ae85e55754d32e8f51ffe3e37f
eyad <eyiadhisham@yahoo.com>
2019-12-25 21:42:26 +0200

    walker gets starting point

--- src/app/models/walker.ts
+++ src/app/models/walker.ts
@@ -1,8 +1,11 @@
 import { Point } from './point';
 export class Walker
 {
+  constructor(start:Point)
+  {
+    this.curPoint = start;
+  }
   visited:Point[];
   curPoint:Point;
   moveStack:Point[];
-
 }
