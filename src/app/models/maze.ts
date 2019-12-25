16b1c48b208027a6ce2237039bd9fe95f872c866
eyad <eyiadhisham@yahoo.com>
2019-12-25 21:11:49 +0200

    maze constructing works

--- src/app/models/maze.ts
+++ src/app/models/maze.ts
@@ -5,78 +5,63 @@ export class Maze
 {
   goal:Tile;
   start:Tile;
-  width:number = 4;
-  height:number = 5;
-  maze:Tile[] = [
-    new Tile(State.open,new Point(0,0)),
-    new Tile(State.open,new Point(0,1)),
-    new Tile(State.open,new Point(0,2)),
-    new Tile(State.open,new Point(0,3)),
-    new Tile(State.open,new Point(0,4)),
+  width:number;
+  height:number;
+  maze:Tile[] = [];
 
-    new Tile(State.open,new Point(1,0)),
-    new Tile(State.blocked,new Point(1,1)),
-    new Tile(State.open,new Point(1,2)),
-    new Tile(State.blocked,new Point(1,3)),
-    new Tile(State.blocked,new Point(1,4)),
+  constructor(width:number,height:number)
+  {
+    this.width = width;
+    this.height = height;
+    for (let x = 0; x < width; x++)
+      for (let y = 0; y < height; y++)
+        this.maze.push(new Tile(State.open,new Point(x,y)));
 
-    new Tile(State.open,new Point(2,0)),
-    new Tile(State.blocked,new Point(2,1)),
-    new Tile(State.open,new Point(2,2)),
-    new Tile(State.open,new Point(2,3)),
-    new Tile(State.open,new Point(2,4)),
 
-    new Tile(State.open,new Point(3,0)),
-    new Tile(State.blocked,new Point(3,1)),
-    new Tile(State.open,new Point(3,2)),
-    new Tile(State.blocked,new Point(3,3)),
-    new Tile(State.goal,new Point(3,4)),
+    this.TileAt(1,1).state = State.blocked;
+    this.TileAt(2,1).state = State.blocked;
+    this.TileAt(3,1).state = State.blocked;
+    this.TileAt(1,3).state = State.blocked;
+    this.TileAt(1,4).state = State.blocked;
+    this.TileAt(3,3).state = State.blocked;
+    this.TileAt(3,4).state = State.goal;
+  }
 
-    new Tile(State.open,new Point(4,0)),
-    new Tile(State.open,new Point(4,1)),
-    new Tile(State.open,new Point(4,2)),
-    new Tile(State.open,new Point(4,3)),
-    new Tile(State.open,new Point(4,4)),
-  ];
-
-  // constructor(width:number,height:number)
-  // {
-  //  for (let row = 0; row < width; row++)
-  //   for (let col = 0; col < height; col++)
-  //   {
-  //     // this.maze.push(new Tile(State.open,new Point(row,col)));
-  //   }
-  // }
-
+  TileAt(x:number,y:number) :Tile
+  {
+    let index = this.to1D(x,y);
+    console.log(this.maze.length);
+    return this.maze[index];
+  }
   Draw()
   {
     let mazeTxt = '';
-    for (let row = 0; row < this.width; row++)
-      for (let col = 0; col < this.height; col++)
+    for (let x = 0; x < this.width; x++)
+    {
+      for (let y = 0; y < this.height; y++)
       {
-        let tile = this.maze[this.to1D(row,col)];
-        switch (tile.state)
-        {
-          case State.open:
-            mazeTxt+='-';
-            break;
-          case State.goal:
+        let index = this.to1D(x,y);
+        console.log(`(${x},${y}) => ${index}`);
+
+        let tile = this.maze[index];
+        if (tile.state == State.open)
+          mazeTxt+='-';
+        if (tile.state == State.goal)
             mazeTxt+='G';
-            break;
-          case State.blocked:
-            mazeTxt+='x';
-            break;
-          default:
-            break;
-        }
-        mazeTxt+='\n';
+        if (tile.state == State.blocked)
+          mazeTxt+='x';
       }
+      mazeTxt+='\n';
+    }
+      // debugger
+      console.log(mazeTxt);
   }
 
   to1D(x:number,y:number) : number
   {
     return y*this.width+x;
-  }  // solveMaze()
+  }
+  // solveMaze()
   // {
   //   let start = new Point(0,0);
 
