7e9c4a5bb8334bf5461ee0f4851c701107f83a67
eyad <eyiadhisham@yahoo.com>
2019-12-25 23:44:13 +0200

    more helper funcitons

--- src/app/models/point.ts
+++ src/app/models/point.ts
@@ -7,8 +7,14 @@ export class Point
   }
   x:number;
   y:number;
+
   toString():string
   {
     return `(${this.x},${this.y})`;
+  }
+
+  static TotalDifference(p1:Point,p2:Point) : number
+  {
+    return (p1.x + p1.y) - (p2.x+p2.y);
   }
 }
