e7d919af59c56ac017d1e2bbf26a200860b544bb
eyad <eyiadhisham@yahoo.com>
2019-12-25 23:45:21 +0200

    changed goal and walker to booleans instead of states

--- src/app/models/tile.ts
+++ src/app/models/tile.ts
@@ -6,13 +6,29 @@ export class Tile
   {
     this.state = state;
     this.coordinate=coordinate;
+    this.hasWalker = false;
+    this.isGoal = false;
+  }
+  SetAsGoal() : void
+  {
+    if(this.state == State.blocked)
+      throw `Can't set tile at ${this.coordinate} to goal because it's blocked`;
+    this.isGoal = true;
+  }
+  RemoveGoal() : void
+  {
+    this.isGoal = false;
+  }
+  IsGoal() : boolean {
+    return this.isGoal
   }
   state:State;
   coordinate:Point;
+  hasWalker:boolean;
+  private isGoal:boolean;
 }
 export enum State
 {
   open,
-  goal,
-  blocked
+  blocked,
 }
