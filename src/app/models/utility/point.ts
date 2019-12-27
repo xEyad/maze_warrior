45792afafeb8f16fc7ba2c4de53ec280507bf43c
eyad <eyiadhisham@yahoo.com>
2019-12-27 19:17:55 +0200

    equal function

--- src/app/models/utility/point.ts
+++ src/app/models/utility/point.ts
@@ -12,7 +12,10 @@ export class Point
   {
     return `(${this.x},${this.y})`;
   }
-
+  Equals(p:Point) : boolean
+  {
+    return p.x == this.x && p.y == this.y;
+  }
   static TotalDifference(p1:Point,p2:Point) : number
   {
     return Math.abs((p1.x + p1.y) - (p2.x+p2.y));
