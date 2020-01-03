da1d5f681c7d0e940e3aa6d72e35aa0239268f0f
eyad <eyiadhisham@yahoo.com>
2020-01-03 22:32:52 +0200

    added 3 more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
-import { GameMetaService } from './services/game-meta.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms'
 
+import { GameMetaService } from './services/game-meta.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BacktrackLogicComponent } from './backtrack-logic/backtrack-logic.component';
@@ -13,6 +14,8 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { ControlPanelComponent } from './ui/control-panel/control-panel.component';
 import { HomePageComponent } from './ui/home-page/home-page.component';
 import { NavbarComponent } from './ui/navbar/navbar.component';
+import { WorldGeneratorComponent } from './ui/world-generator/world-generator.component';
+import { MazeComponent } from './graphics/maze/maze.component';
 
 @NgModule({
   declarations: [
@@ -23,12 +26,15 @@ import { NavbarComponent } from './ui/navbar/navbar.component';
     WorldComponent,
     ControlPanelComponent,
     HomePageComponent,
-    NavbarComponent
+    NavbarComponent,
+    WorldGeneratorComponent,
+    MazeComponent
   ],
   imports: [
     MDBBootstrapModule.forRoot(),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [GameService,GameMetaService],
   bootstrap: [AppComponent]
