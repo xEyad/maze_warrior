edb5ac467b6737ee23123ca8413440783d6209f2
eyad <eyiadhisham@yahoo.com>
2020-01-03 22:34:02 +0200

    world component delegates drawing to maze component

--- src/app/graphics/world/world.component.ts
+++ src/app/graphics/world/world.component.ts
@@ -1,7 +1,6 @@
 import { World } from '../../models/world';
 import { Tile } from '../../models/tile';
 import { Component, OnInit, Input } from '@angular/core';
-import { Point } from 'src/app/models/utility/point';
 import { BacktrackSolverService } from 'src/app/services/backtrack-solver.service';
 
 @Component({
@@ -12,34 +11,13 @@ import { BacktrackSolverService } from 'src/app/services/backtrack-solver.servic
 export class WorldComponent implements OnInit {
 
   @Input() world : Readonly<World>;
-  @Input() indexedLocations:Readonly<Point>[];
   @Input() solver:Readonly<BacktrackSolverService>;
 
-  tiles:Readonly<Tile>[];
-  width:Array<number>;
-  height:Array<number>;
   constructor(){}
 
   ngOnInit()
   {
-    this.width = Array(this.world.Maze().width).fill(0);
-    this.height = Array(this.world.Maze().height).fill(0);
+
   }
-  GetTileAt(x:number,y:number) :Readonly<Tile>
-  {
-    return this.world.Maze().tiles[this.world.Maze().to1D(x,y)];
-  }
-  IsStart(x:number,y:number) : boolean
-  {
-    return this.world.startPos.Equals(this.GetTileAt(x,y).coordinate);
-  }
-  IsIndexed(x:number,y:number) : boolean
-  {
-    for (const p of this.solver.GetIndexedLocations())
-    {
-      if(p.Equals(this.GetTileAt(x,y).coordinate))
-        return true;
-    }
-    return false;
-  }
+
 }
