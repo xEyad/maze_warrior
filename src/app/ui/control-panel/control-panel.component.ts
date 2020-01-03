dd3060d53e7b7a1977c9f28cdc998a7c6eba6f10
eyad <eyiadhisham@yahoo.com>
2020-01-03 22:35:16 +0200

    control panel functional skeleton created

--- src/app/ui/control-panel/control-panel.component.ts
+++ src/app/ui/control-panel/control-panel.component.ts
@@ -1,3 +1,4 @@
+import { GameMetaService } from './../../services/game-meta.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,11 +6,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './control-panel.component.html',
   styleUrls: ['./control-panel.component.scss']
 })
+///responsible for setting graphical options of the world
 export class ControlPanelComponent implements OnInit {
 
-  constructor() { }
+  width;
+  height;
+  constructor(public meta:GameMetaService)
+  {
+    this.width = meta.mazeWidth;
+    this.height = meta.mazeHeight;
+  }
 
   ngOnInit() {
   }
-
+  CreateMaze()
+  {
+    this.meta.SetMazeDimensions(parseInt(this.width),parseInt(this.height));
+  }
 }
