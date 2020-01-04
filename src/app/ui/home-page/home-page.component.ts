8fa9d9dbe503366470eea109a98d68bbf3ea1fdc
eyad <eyiadhisham@yahoo.com>
2020-01-04 21:32:53 +0200

    home page correctly switch to solver when prompted to

--- src/app/ui/home-page/home-page.component.ts
+++ src/app/ui/home-page/home-page.component.ts
@@ -1,3 +1,4 @@
+import { GameMetaService } from './../../services/game-meta.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public meta:GameMetaService) { }
 
   ngOnInit() {
   }
