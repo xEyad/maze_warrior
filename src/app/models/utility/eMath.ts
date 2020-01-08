dd65342597a983f9750d30dfc2e440e7cfb58c9b
eyad <eyiadhisham@yahoo.com>
2020-01-08 23:54:52 +0200

    some utility math functions

---
+++ src/app/models/utility/eMath.ts
@@ -0,0 +1,10 @@
+class Emath
+{
+  ///The maximum is exclusive and the minimum is inclusive
+  static GetRandomInt(min, max)
+  {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+}
